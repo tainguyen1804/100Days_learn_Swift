c895b8ecb8463fa25835946ec9cf2d53c7129310
tainguyen <nguyentantai1804@gmail.com>
2020-12-27 07:57:30 -0800

    day 4

---
+++ Day_4/For_loops.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+let count = 1..<10
+
+for number in count {
+    print("number \(number)")
+}
+
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+print("Players gonna ")
+
+for _ in 1...5 {
+    print("play")
+}
+
+let names = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+
+for _ in names {
+    print("[CENSORED] is a secret agent!")
+}
