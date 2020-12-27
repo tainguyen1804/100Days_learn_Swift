1ee1fbe602aaba70bdf94c94b9f975a139f585b4
tainguyen <nguyentantai1804@gmail.com>
2020-12-27 07:56:44 -0800

    day 2

---
+++ Day_2/Array.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+/*
+let a = "Nguyen Van A"
+let b = "Nguyen Van B"
+let c = "Nguyen Van C"
+
+let arrayString = [a,b,c]
+
+arrayString[2]
+ */
+let arrayInt:[Int] = [2,5,7,8,9]
+
+arrayInt[3]
+ 
+
+//var hello = "hello world"
